3f95a0a29af740bb25a79f37fe05ba924eeabd1e
flishwnag <flish_wang@sina.com>
2018-04-25 14:01:23 +0800

    init

--- demo.py
+++ demo.py
@@ -5,9 +5,11 @@ import os,sys
 import tornado.options
 curdir=os.path.dirname(os.path.abspath(__file__))
 
+
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
         return self.get_secure_cookie("username")
+
 
 class LoginHandler(BaseHandler):
     def get(self):
@@ -16,10 +18,12 @@ class LoginHandler(BaseHandler):
         self.set_secure_cookie("username", self.get_argument("username"))
         self.redirect("/")
 
+
 class WelcomeHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         self.render('index.html', user=self.current_user)
+
 
 class LogoutHandler(BaseHandler):
     def get(self):
@@ -27,9 +31,18 @@ class LogoutHandler(BaseHandler):
             self.clear_cookie("username")
             self.redirect("/")
 
-class MainHandler(tornado.web.RequestHandler):
+
+class DemoHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
+
+
+
+class MyStaticFileHandler(BaseHandler,tornado.web.StaticFileHandler):
+   @tornado.web.authenticated
+   def _get(self, path, include_body=True):
+       return self.get(path,include_body)
+   get=_get
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
@@ -39,13 +52,16 @@ if __name__ == "__main__":
         "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
         "xsrf_cookies": True,
         "login_url": "/login",
+        "static_path": os.path.join(curdir,'static')
     }
 
     application = tornado.web.Application([
         (r'/', WelcomeHandler),
         (r'/login', LoginHandler),
-        (r'/logout', LogoutHandler)
+        (r'/logout', LogoutHandler),
+        (r'/(.*?)$', tornado.web.StaticFileHandler,{'path':os.path.join(curdir,'static'),'default_filename':'index.html'})
     ], **settings)
+
 
     server=tornado.httpserver.HTTPServer(application,ssl_options={
         "certfile":os.path.join(curdir,"ssl/auth.crt"),
