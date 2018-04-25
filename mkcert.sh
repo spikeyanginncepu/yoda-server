b9a40fd8bb70afe922936a60da04bf64e7fe20d9
flishwnag <flish_wang@sina.com>
2018-04-25 09:10:17 +0800

    init

---
+++ mkcert.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+cd $(dirname $0)/ssl
+echo "cd to $(dirname $0)"
+# create self-signed server certificate:
+
+read -p "Enter your domain [www.example.com]: " DOMAIN
+
+echo "Create server key..."
+
+openssl genrsa -des3 -out $DOMAIN.key 1024
+
+echo "Create server certificate signing request..."
+
+SUBJECT="/C=US/ST=Mars/L=iTranswarp/O=iTranswarp/OU=iTranswarp/CN=$DOMAIN"
+
+openssl req -new -subj $SUBJECT -key $DOMAIN.key -out $DOMAIN.csr
+
+echo "Remove password..."
+
+mv $DOMAIN.key $DOMAIN.origin.key
+openssl rsa -in $DOMAIN.origin.key -out $DOMAIN.key
+
+echo "Sign SSL certificate..."
+
+openssl x509 -req -days 3650 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
+
+cp $DOMAIN.crt auth.crt
+cp $DOMAIN.key auth.key
