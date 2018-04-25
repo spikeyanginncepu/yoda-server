ebce638ff2a4b85f423e5afb279c6ffc8f449f91
flishwnag <flish_wang@sina.com>
2018-04-25 09:34:14 +0800

    init

--- demo.py
+++ demo.py
@@ -35,11 +35,10 @@ if __name__ == "__main__":
     tornado.options.parse_command_line()
 
     settings = {
-        "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+        "template_path": os.path.join(os.path.dirname(__file__), "static"),
         "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
         "xsrf_cookies": True,
         "login_url": "/login",
-        'path': "static"
     }
 
     application = tornado.web.Application([
