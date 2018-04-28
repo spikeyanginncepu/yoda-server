26181ea9142d25bd9ffe1508cdb787db7f2e921a
llin <linlong_buaa@163.com>
2018-04-28 10:02:30 +0800

    update the page framework

--- static/js/yoda.js
+++ static/js/yoda.js
@@ -3,10 +3,14 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+jQuery.getJSON = function(url, args, callback) {
+	args._xsrf = getCookie("_xsrf");
+    $.ajax({url: url, data: $.param(args), dataType: "text", type: "GET", success: callback});
+};
+
 jQuery.postJSON = function(url, args, callback) {
 	args._xsrf = getCookie("_xsrf");
-    //$.ajax({url: url, data: $.param(args), dataType: "text", type: "POST", 
-    $.ajax({url: url, data: $.param(args), dataType: "text", type: "GET", success: callback});
+    $.ajax({url: url, data: $.param(args), dataType: "text", type: "POST", success: callback});
 };
 
 let subject = null;
@@ -14,12 +18,24 @@ let item = null;
 
 function setNav(response) {
 	element('left').innerHTML = response;
+	let menu = element('leftMenu');
+	menu.rows[0].click();
+}
+
+function setContent(response) {
+	element('content').innerHTML = response;
 }
 
 function navigate(obj) {
 	subject = obj.text();
 	let url = 'html/' + subject + '.html';
-	$.postJSON(url, {}, setNav);
+	$.getJSON(url, {}, setNav);
+}
+
+function leftNav(obj) {
+	item = $("td.dMcol1").first().text();
+	let url = 'html/' + subject + '-' + item + '.html';
+	$.getJSON(url, {}, setContent);
 }
 
 function element(id) {
