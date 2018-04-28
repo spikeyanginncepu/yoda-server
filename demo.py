f84dc2dc0993346876de0c30810a66d7bc9130e3
llin <linlong_buaa@163.com>
2018-04-28 10:24:06 +0800

    update

--- demo.py
+++ demo.py
@@ -3,8 +3,24 @@ import tornado.web
 import tornado.httpserver
 import os,sys
 import tornado.options
+import logging
+import signal
+
 curdir=os.path.dirname(os.path.abspath(__file__))
 
+is_closing = False
+
+def signal_handler(signum, frame):
+    global is_closing
+    logging.info('exiting...') 
+    is_closing = True
+
+def try_exit(): 
+    global is_closing 
+    if is_closing:
+        # clean up here 
+        tornado.ioloop.IOLoop.instance().stop() 
+        logging.info('exit success')
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -16,14 +32,13 @@ class LoginHandler(BaseHandler):
         self.render('login.html')
     def post(self):
         self.set_secure_cookie("username", self.get_argument("username"))
-        self.redirect("/")
+        self.redirect("main.html")
 
 
 class WelcomeHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
-        self.render('index.html', user=self.current_user)
-
+        self.render('main.html', user=self.current_user)
 
 class LogoutHandler(BaseHandler):
     def get(self):
@@ -37,7 +52,6 @@ class DemoHandler(tornado.web.RequestHandler):
         self.write("Hello, world")
 
 
-
 class AuthStaticFileHandler(BaseHandler,tornado.web.StaticFileHandler):
     def get_current_user(self):
         return BaseHandler.get_current_user(self)
@@ -47,8 +61,8 @@ class AuthStaticFileHandler(BaseHandler,tornado.web.StaticFileHandler):
         return tornado.web.StaticFileHandler.get(self,*args,**kwargs)
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
-
+    tornado.options.parse_command_line() 
+    signal.signal(signal.SIGINT, signal_handler)
     settings = {
         "template_path": os.path.join(os.path.dirname(__file__), "static"),
         "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
@@ -58,11 +72,11 @@ if __name__ == "__main__":
     }
 
     application = tornado.web.Application([
-        (r'/', WelcomeHandler),
+        #(r'/', WelcomeHandler),
         (r'/login', LoginHandler),
         (r'/logout', LogoutHandler),
         (r'/data/(.*?)$', AuthStaticFileHandler,{'path':os.path.join(curdir,'data')}),
-        (r'/(.*?)$', tornado.web.StaticFileHandler,{'path':os.path.join(curdir,'static'),'default_filename':'index.html'})
+        (r'/(.*?)$', tornado.web.StaticFileHandler,{'path':os.path.join(curdir,'static'),'default_filename':'static/main.html'})
     ], **settings)
 
 
@@ -71,4 +85,5 @@ if __name__ == "__main__":
         "keyfile":os.path.join(curdir,"ssl/auth.key")
         })
     server.listen(8888)
+    tornado.ioloop.PeriodicCallback(try_exit, 100).start()
     tornado.ioloop.IOLoop.instance().start()
