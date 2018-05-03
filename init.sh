51008e8607ddd927a99863aa91a16728cc9c5f84
flishwnag <flish_wang@sina.com>
2018-05-03 16:32:34 +0800

    restructed

--- init.sh
+++ init.sh
@@ -9,7 +9,6 @@ mkdir -p data/admin
 #init postgresql
 echo "create user yoda" | sudo sudo -u postgres psql
 echo "alter user yoda with password '`cat ssl/password2.txt`'" | sudo sudo -u postgres psql
-sudo sudo -u postgres psql -f initdb.sql
 echo "CREATE DATABASE yoda OWNER yoda;" | sudo -u postgres psql -f initdb.sql
 echo "GRANT ALL PRIVILEGES ON DATABASE yoda to yoda;" | sudo -u postgres psql -f initdb.sql
 sudo -u postgres psql yoda -f initdb.sql
