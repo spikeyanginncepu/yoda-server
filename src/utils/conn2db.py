841a552134ef9e1513651652e758514b4b092690
flishwnag <flish_wang@sina.com>
2018-05-09 16:34:20 +0800

    restructed

--- src/utils/conn2db.py
+++ src/utils/conn2db.py
@@ -4,5 +4,6 @@ def conn2db(config):
     conn=psycopg2.connect(host=config.db.get('host',None), database=config.db.get('database',None),
                             user=config.db.get('user',None), password=config.db.get('password',None))
     conn.autocommit = True
+    conn.set_client_encoding('UTF8')
     return conn
 
